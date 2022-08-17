f48e0fa9b21dce582eff87d570ce4cbcdf13d3fc
Matsashi <matthew.pottier@hotmail.fr>
2022-08-17 18:33:56 +0200

    finished tuto

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card = ({ country }) => {
+  return (
+    <li className="card">
+      <img
+        src={country.flags.svg}
+        alt={"drapeau" + country.translations.fra.common}
+      />
+      <div className="infos">
+        <h2>{country.translations.fra.common}</h2>
+        <p>Capital : {country.capital}</p>
+        <p>Population : {country.population.toLocaleString()}</p>
+      </div>
+    </li>
+  );
+};
+
+export default Card;
---
+++ src/pages/About.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum atque
+        inventore obcaecati aspernatur aliquam optio expedita. Quia quae libero
+        id, optio sint, modi eaque in, quam ipsa nobis beatae et? Ratione est
+        nostrum sunt magnam rerum odio perferendis quos laboriosam, consequuntur
+        at temporibus illum, asperiores nesciunt, officia possimus inventore
+        quaerat vitae sequi voluptatum unde reiciendis vero quae! Tenetur ex
+        enim nobis. Minus tempora nemo autem, exercitationem dolores atque nisi
+        impedit cum ea labore necessitatibus ut sed a earum, voluptas et, ex
+        explicabo ab sequi natus. Saepe et nisi accusamus repudiandae. Omnis
+        numquam eos, reiciendis cum aspernatur unde ratione incidunt. Qui?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus,
+        dolorem doloribus quis quam minima quod deserunt, quia commodi ipsam,
+        molestiae perspiciatis reprehenderit totam autem sequi fugiat beatae
+        asperiores expedita cum consectetur aperiam vitae. Labore earum
+        perspiciatis iure magni sed sunt dolores odit sapiente consequuntur ab,
+        soluta itaque quos placeat iste dolor ullam cupiditate nesciunt nam quae
+        voluptatum fugiat reprehenderit et similique dolore? Officiis, accusamus
+        tempore! Vero soluta nemo doloremque ipsa aut ratione, consectetur nulla
+        atque, ad voluptatibus magni quidem, corrupti dolores quasi recusandae
+        omnis consequatur placeat consequuntur. Beatae, dolorum velit dicta
+        ullam, dignissimos molestiae esse illum totam ut nobis impedit natus
+        maiores commodi quae laboriosam nam quis nisi cumque at? Perferendis
+        facere officia ducimus excepturi esse perspiciatis vero aspernatur
+        dolores dolorem consequatur eius natus enim sint praesentium asperiores,
+        corrupti eveniet! Recusandae quibusdam ipsam explicabo quisquam
+        doloremque delectus distinctio perferendis, quaerat accusantium ea alias
+        commodi doloribus magni tempora atque mollitia nulla molestiae, nemo
+        aliquid harum repudiandae earum natus impedit corrupti. Modi culpa
+        repellat alias magni autem perferendis assumenda debitis soluta quae,
+        molestias in laboriosam aut. Nihil pariatur tempore necessitatibus ex
+        tempora modi eaque explicabo distinctio repellat, molestias voluptates
+        optio natus placeat nulla ducimus dolore velit voluptatum asperiores ab
+        rem eligendi. Aperiam.
+      </p>
+    </div>
+  );
+};
+
+export default About;
